3cfacab61d872886e92c67841334ccd408c1ea7a
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2018-11-18 18:00:36 +0100

    Add member

--- stemmata/tree/models.py
+++ stemmata/tree/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Member(models.Model):
+    FEMALE = 'FE'
+    MALE = 'MA'
+    GENDERS = (
+        (FEMALE, 'Female'),
+        (MALE, 'Male'),
+    )
+    parent_1 = models.OneToOneField('self', null=True, blank=True, on_delete=models.SET_NULL)
+    parent_2 = models.OneToOneField('self', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=255, blank=False)
+    gender = models.CharField(max_length=1, blank=True, choices=GENDERS,
+                              default=FEMALE)
